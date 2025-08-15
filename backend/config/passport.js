cb8e96014c83cd741f427504074a26cf65a62453
massoncorlette <massoncorlette07@gmail.com>
2025-08-15 01:17:23 -0400

    feat(config/passport): adding login passport strategy

---
+++ backend/config/passport.js
@@ -0,0 +1,71 @@
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
+const LocalStrategy = require("passport-local").Strategy;
+const pool = require("../db/pool");
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query(
+        "SELECT * FROM users WHERE email = $1",
+        [username],
+      );
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect email" });
+      }
+
+      //comparing login password to hashed stored password
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        // passwords do not match!
+        return done(null, false, { message: "Incorrect password" });
+      }
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }),
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.user_id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query(
+      "SELECT * FROM users WHERE user_id = $1",
+      [id],
+    );
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+const { validationResult } = require("express-validator");
+
+const authenticateUser = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.locals.errorMsg = "Wrong email or password.";
+
+    return res.status(400).render("index", {
+      errors: errors.array(),
+    });
+  }
+
+  // If no validation errors, authenticate with passport
+  passport.authenticate("local", {
+    successRedirect: "/home",
+    failureRedirect: "/",
+  })(req, res, next);
+};
+
+module.exports = { authenticateUser };
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,4 @@
 // const authenticateUser = require("../config/passport");
-const jwt = require('jsonwebtoken');
 
 const { Router } = require("express");
 
@@ -9,31 +8,11 @@ indexRouter.get("/", async (req, res, next) => {
   res.send("get Index");
 });
 
-indexRouter.post("/login", (req, res) => {
+const {authenticateUser} = require('./config/passport');
+const {validateUser} = require('./controllers/viewController')
 
-  //parse req.body from React component
+indexRouter.post("/login", validateUser(), authenticateUser);
 
-  const user = {
-    id: 1,
-    username: 'brad',
-    email: 'brad@gmail.com'
-  }
-
-
-  // sends user obj, secretkey and exp as payload
-  jwt.sign({user:user}, 'secretkey', { expiresIn: '2 days' }, (err,token) => {
-    res.json({
-      token:token
-    });
-  });
-
-  res.redirect("/home");
-});
-
-
-
-
-// indexRouter.post("/", authenticateUser);
 
 
 module.exports = {indexRouter};
