4d213be6e7e6393f6b6a240de9d3363ee5573680
massoncorlette <massoncorlette07@gmail.com>
2025-08-16 23:49:35 -0400

    feat(routes/home): home route signs token and sets in authorization header for client side user obj

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -5,12 +5,14 @@ var jwt = require('jsonwebtoken');
 
 homeRouter.get('/', (req, res) => {
 
-    // payloads user object, last argument callback for generated token
-    jwt.sign(req.user, 'secretkey', { expiresIn: '2 days' }, (err,token) => {
-      res.json({
-        token:token
-      });
-    });
+  // payloads user object, last argument callback for generated token
+  jwt.sign(req.user, 'secretkey', { expiresIn: '2 days' }, (err,token) => {
+
+    res.setHeader('Content-Type', 'application/json');
+
+    res.setHeader("Authorization", `Bearer ${token}`);
+    
+  });
 });
 
 module.exports = {homeRouter}
